dfed3c1f116226805d2d9e73feeb668ae4bd59db
gbrindle <gracebrindle@gmail.com>
2022-04-10 23:21:32 -0400

    Add search_following method

--- final_project.py
+++ final_project.py
@@ -11,10 +11,12 @@ with open("/Users/gracebrindle/Desktop/si507/final_project/politician_dataset.cs
     reader = csv.reader(f)
     data = list(reader) 
 
-class_list = []
+# Initialize global variables
+class_dict = {}
+bearer_token = TwitterKeys.Bearer_Token
 
 class Politician:
-     def __init__(self, name="No Name", twitter_username="No username", account_id = "No ID", sex="Unknown", birthplace="United States of America", age="Unknown", political_party="Unknown"):
+    def __init__(self, name="No Name", twitter_username="No username", account_id = "No ID", sex="Unknown", birthplace="United States of America", age="Unknown", political_party="Unknown"):
          self.name = name
          self.twitter_username = twitter_username
          self.account_id = account_id
@@ -23,90 +25,87 @@ class Politician:
          self.age = age
          self.political_party = political_party 
 
+    # Searches for who a politician follows (aka their "friends")
+    def search_following(self):
+        search_url = "https://api.twitter.com/1.1/friends/ids.json"
+        params = self.account_id
+        response = requests.get(search_url, auth=bearer_oauth, params=params)
+        print(response.status_code)
+        if response.status_code != 200:
+            raise Exception(response.status_code, response.text)
+        return response.json()
+
+# Loop through the data and create a Politician object for each row of data
 for politician in data[1:]:
     new_politician = Politician(politician[0], politician[1], politician[3], politician[4], politician[5], politician[7], politician[9])
-    class_list.append(new_politician)
+    # Append the Politician class to a dictionary
+    class_dict[new_politician.name] = new_politician
 
-
-def prompt(class_list):
+# Prompt the user to search for a politician
+def prompt(class_dict):
     
     print(" ")
-    politician = input("Please enter the name of the politician you would like to search or 'exit' to quit: ")
+    politician = input("Please enter the name of the American politician you would like to search or 'exit' to quit: ")
 
     if politician == "exit":
         exit()
     
     else:
-        # search(politician, data_dict)
-        pass
+        search(politician, class_dict)
 
 # # Implement a breadth-first search function
-# def search(politician, data_dict):
+def search(politician, class_dict):
     
-#     visited_set = set()
-#     queue = deque()
+    visited_set = set()
+    queue = deque()
 
-#     if politician in data_dict:
+    if politician in class_dict:
 
-#         print("")
-#         print("Commencing search...")
-#         print("")
+        print("")
+        print("Commencing search...")
+        print("")
+
+        # Fetch a list of friends from the Twitter API using the account ID
+        class_dict[politician].search_following()
         
-#         for movie in data_dict[politician]:
-#             queue.append((movie, [movie]))
-#             visited_set.add(movie)
+    #     for movie in class_dict[politician]:
+    #         queue.append((movie, [movie]))
+    #         visited_set.add(movie)
         
-#         while queue:
-#             data = queue.popleft()
-#             data_name = data[0]
-#             data_path = data[1]
+    #     while queue:
+    #         data = queue.popleft()
+    #         data_name = data[0]
+    #         data_path = data[1]
             
-#             if data_name == actorB:
-#                 print("Found actor link")
-#                 print("")
-#                 print("Path from " + actorA + " to " + actorB + ": " + actorA + "-> " + "-> ".join(data_path))
-#                 count = len(data_path) // 2
-#                 print("Degrees of separation from " + actorB + ": " + str(count))
-#                 print("")
-#                 prompt(data_dict)
-#                 return data_path
+    #         if data_name == actorB:
+    #             print("Found actor link")
+    #             prompt(data_dict)
+    #             return data_path
 
-#             else:
-#                 for i in data_dict[data_name]:
-#                     if i not in visited_set:
-#                         queue.append((i, data_path + [i]))
-#                         visited_set.add(i)
+    #         else:
+    #             for i in class_dict[data_name]:
+    #                 if i not in visited_set:
+    #                     queue.append((i, data_path + [i]))
+    #                     visited_set.add(i)
         
-#     else:
-#         print("")
-#         print("Error: Could not locate the politician's Twitter account. Please re-enter the politician's name")
-#         print("")
-#         prompt(data_dict)
+    else:
+        print("")
+        print("Error: Could not locate the politician's Twitter account. Please re-enter the politician's name")
+        print("")
+        prompt(class_dict)
 
-# bearer_token = TwitterKeys.Bearer_Token
-
-# def bearer_oauth(r):
-#     """
-#     Method required by bearer token authentication.
-#     """
-#     r.headers["Authorization"] = f"Bearer {bearer_token}"
-#     r.headers["User-Agent"] = "v2RecentSearchPython"
-#     return r
-
-# # Searches for who a user follows (aka their "friends")
-# def search_following(account_id):
-#     search_url = "https://api.twitter.com/1.1/friends/ids.json"
-#     params = account_id
-#     response = requests.get(search_url, auth=bearer_oauth, params=params)
-#     print(response.status_code)
-#     if response.status_code != 200:
-#         raise Exception(response.status_code, response.text)
-#     return response.json()
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2RecentSearchPython"
+    return r
 
 def main():
     # json_response = search_following('18189966')
     # print(json.dumps(json_response, indent=4, sort_keys=True))
-    prompt(class_list)
+    prompt(class_dict)
     
 if __name__ == "__main__":
     main()
